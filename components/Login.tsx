52c459b55e6590fb1f93c4973dcfd2e3c5d1a0e8
tharitnut <nong.peanut.butter.mail@gmail.com>
2024-08-10 09:56:23 +0700

    Practice4_TextInput Component

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,13 +1,55 @@
-import { View, Text, TextInput,Button, Alert } from "react-native";
-import React,{useState} from "react";
+import { View, Text, TextInput, Button, Alert } from "react-native";
+import React, { useState } from "react";
 import { stylesLogin } from "../styles/styles";
 
-const Login = () => {
+const Login = (): React.JSX.Element => {
   const [fullname, setFullname] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const ValildateEmail = (email: string): boolean => {
+    const recheckEmail = /\S+@\S+\.\S+/;
+    return recheckEmail.test(email);
+  };
 
   const handleSubmit = () => {
-    !fullname?Alert.alert("Please Enter Name"):!email?Alert.alert("Please Enter Email"):Alert.alert("Success");
+    let errorMessage = "";
+    if (!fullname) {
+      errorMessage = "Please Enter Name\n";
+    }
+    if (!email) {
+      errorMessage += "Please Enter Email\n";
+    }else if(!ValildateEmail(email)){
+      errorMessage += "Invalid Email Format\n";
+    }
+    if (!password) {
+      errorMessage += "Please Enter Password\n";
+    } else if (password.length < 6) {
+      errorMessage += "Password must be at least 6 letter";
+    }
+    if (errorMessage) {
+      Alert.alert("Error", errorMessage.trim(), [{ text: "OK" }]);
+      return;
+    }
+    Alert.alert("Alert", "Success", [{ text: "OK" }]);
+
+    /* if(!fullname&&!email){
+      Alert.alert("Error", "Please Enter Name\nPlease Enter Email", [{ text: "OK" }]);
+      return;
+    }
+    if (!fullname) {
+      Alert.alert("Alert", "Please Enter Name", [{ text: "OK" }]);
+      return; //break
+    }
+    if (!email) {
+      Alert.alert("Alert", "Please Enter Email", [{ text: "OK" }]);
+      return;
+    }
+    Alert.alert("Alert", "Success", [{ text: "OK" }]); */
+
+    /* !fullname?Alert.alert("Please Enter Name","Please Enter Name",[{text:"OK"}])
+    :!email?Alert.alert("Please Enter Email")
+    :Alert.alert("Success"); */
   };
 
   return (
@@ -23,8 +65,16 @@ const Login = () => {
         placeholder="Enter Email"
         value={email}
         onChangeText={setEmail}
+        keyboardType="email-address"
       />
-      <Button title="Submit" onPress={handleSubmit} color="blue" />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+      <Button title="SUBMIT" onPress={handleSubmit} color="blue" />
     </View>
   );
 };
